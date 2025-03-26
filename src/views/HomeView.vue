a5aa81e4be0a7d177a57cf02f9fea7e4afb081d5
Petar Kresoja <pkresoja@singidunum.ac.rs>
2025-03-26 15:23:10 +0100

    Add cards for flight display

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,33 @@ FlightService.getFlights()
 </script>
 
 <template>
-    <ul>
-        <li v-for="f of flights" :key="f.id">{{ f.destination }} {{ f.flightNumber }} {{ f.plane }}</li>
-    </ul>
+    <div class="wrapper mb-3 mt-3">
+        <div class="card" style="width: 18rem;" v-for="f of flights" :key="f.id">
+            <img :src="`https://img.pequla.com/destination/${f.destination.toLowerCase().split(' ')[0]}.jpg`"
+                class="card-img-top" :alt="f.destination">
+            <div class="card-body">
+                <h5 class="card-title">{{ f.destination }}</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">{{ f.flightNumber }}</h6>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">An item</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item">A third item</li>
+            </ul>
+            <div class="card-body">
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
+    </div>
 </template>
+
+<style>
+.wrapper {
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+</style>
